c939d987456c990e39a59111bf83c5c0a808054a
Amrutha Korumilli <34261526+Amrutha-korumilli@users.noreply.github.com>
2025-07-13 23:19:17 -0400

    Revert "Tools calling"

--- prisma/migrations/20250714002730_add_last_message_id/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Task" DROP CONSTRAINT "Task_userId_fkey";
-
--- AlterTable
-ALTER TABLE "Task" ADD COLUMN     "lastMessageId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "Task" ADD CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
